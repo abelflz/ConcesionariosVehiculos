69a22db4c664b329d24dca246b49300a0152e324
AbelFlz <abelfeliz@gmail.com>
2017-10-25 19:31:38 -0400

    Código que envía log a db en caso de excepción

--- ConcesionariosVehiculos/FrmLogin.cs
+++ ConcesionariosVehiculos/FrmLogin.cs
@@ -18,35 +18,56 @@ namespace ConcesionariosVehiculos
             InitializeComponent();
         }
 
+        private string x = System.Configuration.ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
         private void validacion() {
             string user = txtUsername.Text;
             string password = txtPassword.Text;
-            var x = System.Configuration.ConfigurationManager.ConnectionStrings["db"].ConnectionString;
 
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = x;
-            con.Open();
-
-            string query = "SELECT COUNT(*) FROM Concesionario WHERE Usuario IN(@User) AND Clave IN(@Password)";
-            SqlCommand cmd = new SqlCommand(query, con);
-
-            cmd.Parameters.Add(new SqlParameter("@User", user));
-            cmd.Parameters.Add(new SqlParameter("@Password", password));
-
-            var CompareValue = cmd.ExecuteScalar();
-
-            string compare = CompareValue.ToString();
-
-            con.Close();
-
-            if (compare.Equals("1"))
+            try
             {
-                MessageBox.Show("Bien, pasó");
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = x;
+                con.Open();
+
+                string query = "SELECT COUNT(*) FROM Concesionario WHERE Usuario IN(@User) AND Clave IN(@Password)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.Add(new SqlParameter("@User", user));
+                cmd.Parameters.Add(new SqlParameter("@Password", password));
+
+                var CompareValue = cmd.ExecuteScalar();
+
+                string compare = CompareValue.ToString();
+
+                con.Close();
+
+                if (compare.Equals("1"))
+                {
+                    /************************************************Llamar al otro formulario******************************************/
+                    MessageBox.Show("Bien, pasó");
+                }
+                else
+                {
+                    MessageBox.Show("Contraseña/Usuario digitado de forma errónea, o cuenta no existente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            else
-            {
-                MessageBox.Show("Contraseña/Usuario digitado de forma errónea, o cuenta no existente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            catch (Exception msg) {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = x;
+
+                string eMessage = msg.ToString();
+                con.Open();
+
+                string query = "INSERT INTO LOGS VALUES(@logInfo, GETDATE())";
+                SqlCommand cmd = new SqlCommand(query,con);
+                cmd.Parameters.Add(new SqlParameter("@logInfo", eMessage));
+                MessageBox.Show("No se pudo completar solicitud, favor contactar al proveedor", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            } 
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
